cae1a08dc2763e68715cfc32f72942551a37f67d
haugaard.simon <haugaard.simon@cs.ucalgary.ca>
2017-03-08 11:04:16 +0100

    Added Visitor, Behavior and BehaviorTypes

---
+++ RecommendationAPI/src/RecommendationAPI/Business/Visitor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecommendationAPI.Business
+{
+    public class Visitor
+    {
+        private string _UID;
+        private string _ProfileUID;
+        private string _CustomerUID;
+
+        public string UID
+        {
+            get
+            {
+                return _UID;
+            }
+
+            set
+            {
+                _UID = value;
+            }
+        }
+
+        public string ProfileUID
+        {
+            get
+            {
+                return _ProfileUID;
+            }
+
+            set
+            {
+                _ProfileUID = value;
+            }
+        }
+
+        public string CustomerUID
+        {
+            get
+            {
+                return _CustomerUID;
+            }
+
+            set
+            {
+                _CustomerUID = value;
+            }
+        }
+
+        public Visitor(string uID, string profileUID, string customerUID) {
+            _UID = uID;
+            _ProfileUID = profileUID;
+            _CustomerUID = customerUID;
+        }
+
+
+    }
+}
