e745b6313c94e3cebc7874766d266e2cfcfbf500
haugaard.simon <haugaard.simon@cs.ucalgary.ca>
2017-03-14 12:07:25 +0100

    Cleaned up test class

---
+++ RecommendationAPI/APITest/GetProductRecommendationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+using RecommendationAPI.Business;
+using System.Diagnostics;
+using System.IO;
+using System.Diagnostics.Tracing;
+using Microsoft.Extensions.Logging;
+using Xunit.Sdk;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        public class RepeatAttribute : DataAttribute {
+            private readonly int _count;
+
+            public RepeatAttribute(int count) {
+                if (count < 1) {
+                    throw new ArgumentOutOfRangeException(nameof(count),
+                          "Repeat count must be greater than 0.");
+                }
+                _count = count;
+            }
+
+            public override IEnumerable<object[]> GetData(MethodInfo testMethod) {
+                return Enumerable.Repeat(new object[0], _count);
+            }
+        }
+
+        public Tests() {
+            pr = new ProductRecommender(new DatabaseEngine());
+        }
+    }
+}
