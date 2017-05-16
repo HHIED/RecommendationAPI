eb42c23cadd973bd9706ea27d78b096e0d4c3a5b
lasse <lassebh9@hotmail.com>
2017-05-16 10:38:09 +0200

    Split files into folders

---
+++ RecommendationAPI/src/RecommendationAPI/Communication/IProductRecommender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecommendationAPI.Business
+{
+    interface IProductRecommender
+    {
+        string[] GetProductRecommendations(string VisitorUID, int numberOfRecommendations, string database);
+        void CalculateTopProducts(string visitorUID, string database);
+        void CalculateAllTopProducts(string database);
+        void CalculateMonthlyTop(string database, DateTime from);
+    }
+}
